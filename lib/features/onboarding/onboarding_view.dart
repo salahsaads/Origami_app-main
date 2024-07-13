b5c94e6816f598ebb2ea9829dfa5375ec06b422c
Salah Saad <kassabksab@gmail.com>
2024-07-13 13:53:30 +0300

    add  OnBoardingView in main

--- lib/features/onboarding/onboarding_view.dart
+++ lib/features/onboarding/onboarding_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:origami/core/Theme/constant.dart';
+import 'package:origami/features/login/presation/view/login_screen.dart';
 import 'package:origami/features/onboarding/intro_view.dart';
 
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
@@ -35,7 +36,8 @@ class OnBoardingView extends StatelessWidget {
               subtitle:
                   'يمكنك بسهوله تحديد مواعيد حمع القمامه بحسب جدولك الشخصي ',
               ontap: () {
-                Navigator.pushNamed(context, 'loginpage');
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => LoginScreen()));
               },
             ),
           ],
--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,13 @@ import 'package:origami/bussinus_logic/authentiacation/authentication.dart';
 import 'package:origami/features/Nav/prestion/view/nav_bar.dart';
 
 import 'package:origami/features/login/presation/view/login_screen.dart';
+import 'package:origami/features/onboarding/onboarding_view.dart';
 import 'package:origami/features/register/prestion/view/register_screen.dart';
 import 'package:origami/features/screens/splah_screen.dart';
 
 import 'firebase_options.dart';
-int sum_all=0;
+
+int sum_all = 0;
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
@@ -47,40 +49,35 @@ class CustomMaterialApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
-
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         theme: ThemeData(),
-        home: Builder(
-          builder: (context) {
-            return SplashScreen(
-              nextScreen: FutureBuilder<bool>(
-                  future: checkLoginStatus(),
-                  builder: (context, snapshot) {
-                    if (snapshot.connectionState ==
-                        ConnectionState.waiting) {
-                      // Show loading indicator or splash screen while checking login status
-                      return const CircularProgressIndicator();
+        home: Builder(builder: (context) {
+          return SplashScreen(
+            nextScreen: FutureBuilder<bool>(
+                future: checkLoginStatus(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    // Show loading indicator or splash screen while checking login status
+                    return const CircularProgressIndicator();
+                  } else {
+                    if (snapshot.hasError) {
+                      // Handle error
+                      return Text('Error: ${snapshot.error}');
                     } else {
-                      if (snapshot.hasError) {
-                        // Handle error
-                        return Text('Error: ${snapshot.error}');
-                      } else {
-                        // Navigate to home or login screen based on login status
-                        return snapshot.data == true
-                            ? const HomeScreen()
-                            : const LoginScreen();
-                      }
+                      // Navigate to home or login screen based on login status
+                      return snapshot.data == true
+                          ? const HomeScreen()
+                          : OnBoardingView();
                     }
-                  }),
-              routes: {
-                'loginpage': (context) => const LoginScreen(),
-                'registerpage': (context) => const RegisterScreen(),
-                'homepage': (context) => const HomeScreen(),
-              },
-            );
-          }
-        ));
+                  }
+                }),
+            routes: {
+              'loginpage': (context) => const LoginScreen(),
+              'registerpage': (context) => const RegisterScreen(),
+              'homepage': (context) => const HomeScreen(),
+            },
+          );
+        }));
   }
 }
